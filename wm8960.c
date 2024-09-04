c77d6a3513703ce4de6f0b1e716e2d293e5dd593
Alex Amo <hello@alejandroamo.eu>
2024-09-04 03:29:47 +0200

    update kernel interfaces, rebuild dtbo

--- wm8960.c
+++ wm8960.c
@@ -1440,68 +1440,65 @@ static void wm8960_set_pdata_from_of(struct i2c_client *i2c,
 		pdata->shared_lrclk = true;
 }
 
-static int wm8960_i2c_probe(struct i2c_client *i2c)
+static int wm8960_i2c_probe(struct i2c_client *client, const struct i2c_device_id *id)
 {
-	struct wm8960_data *pdata = dev_get_platdata(&i2c->dev);
-	struct wm8960_priv *wm8960;
-	int ret;
+    struct wm8960_data *pdata = dev_get_platdata(&client->dev);
+    struct wm8960_priv *wm8960;
+    int ret;
 
-	wm8960 = devm_kzalloc(&i2c->dev, sizeof(struct wm8960_priv),
-			      GFP_KERNEL);
-	if (wm8960 == NULL)
-		return -ENOMEM;
+    wm8960 = devm_kzalloc(&client->dev, sizeof(struct wm8960_priv), GFP_KERNEL);
+    if (wm8960 == NULL)
+        return -ENOMEM;
 
-	wm8960->clk_id = WM8960_SYSCLK_PLL;
-	wm8960->mclk = devm_clk_get(&i2c->dev, "mclk");
-	if (IS_ERR(wm8960->mclk)) {
-		if (PTR_ERR(wm8960->mclk) == -EPROBE_DEFER)
-			return -EPROBE_DEFER;
-	}
+    wm8960->clk_id = WM8960_SYSCLK_PLL;
+    wm8960->mclk = devm_clk_get(&client->dev, "mclk");
+    if (IS_ERR(wm8960->mclk)) {
+        if (PTR_ERR(wm8960->mclk) == -EPROBE_DEFER)
+            return -EPROBE_DEFER;
+    }
 
-	wm8960->regmap = devm_regmap_init_i2c(i2c, &wm8960_regmap);
-	if (IS_ERR(wm8960->regmap))
-		return PTR_ERR(wm8960->regmap);
+    wm8960->regmap = devm_regmap_init_i2c(client, &wm8960_regmap);
+    if (IS_ERR(wm8960->regmap))
+        return PTR_ERR(wm8960->regmap);
 
-	if (pdata)
-		memcpy(&wm8960->pdata, pdata, sizeof(struct wm8960_data));
-	else if (i2c->dev.of_node)
-		wm8960_set_pdata_from_of(i2c, &wm8960->pdata);
+    if (pdata)
+        memcpy(&wm8960->pdata, pdata, sizeof(struct wm8960_data));
+    else if (client->dev.of_node)
+        wm8960_set_pdata_from_of(client, &wm8960->pdata);
 
-	ret = wm8960_reset(wm8960->regmap);
-	if (ret != 0) {
-		dev_err(&i2c->dev, "Failed to issue reset\n");
-		return ret;
-	}
+    ret = wm8960_reset(wm8960->regmap);
+    if (ret != 0) {
+        dev_err(&client->dev, "Failed to issue reset\n");
+        return ret;
+    }
 
-	if (wm8960->pdata.shared_lrclk) {
-		ret = regmap_update_bits(wm8960->regmap, WM8960_ADDCTL2,
-					 0x4, 0x4);
-		if (ret != 0) {
-			dev_err(&i2c->dev, "Failed to enable LRCM: %d\n",
-				ret);
-			return ret;
-		}
-	}
+    if (wm8960->pdata.shared_lrclk) {
+        ret = regmap_update_bits(wm8960->regmap, WM8960_ADDCTL2, 0x4, 0x4);
+        if (ret != 0) {
+            dev_err(&client->dev, "Failed to enable LRCM: %d\n", ret);
+            return ret;
+        }
+    }
 
-	/* Latch the update bits */
-	regmap_update_bits(wm8960->regmap, WM8960_LINVOL, 0x100, 0x100);
-	regmap_update_bits(wm8960->regmap, WM8960_RINVOL, 0x100, 0x100);
-	regmap_update_bits(wm8960->regmap, WM8960_LADC, 0x100, 0x100);
-	regmap_update_bits(wm8960->regmap, WM8960_RADC, 0x100, 0x100);
-	regmap_update_bits(wm8960->regmap, WM8960_LDAC, 0x100, 0x100);
-	regmap_update_bits(wm8960->regmap, WM8960_RDAC, 0x100, 0x100);
-	regmap_update_bits(wm8960->regmap, WM8960_LOUT1, 0x100, 0x100);
-	regmap_update_bits(wm8960->regmap, WM8960_ROUT1, 0x100, 0x100);
-	regmap_update_bits(wm8960->regmap, WM8960_LOUT2, 0x100, 0x100);
-	regmap_update_bits(wm8960->regmap, WM8960_ROUT2, 0x100, 0x100);
+    /* Latch the update bits */
+    regmap_update_bits(wm8960->regmap, WM8960_LINVOL, 0x100, 0x100);
+    regmap_update_bits(wm8960->regmap, WM8960_RINVOL, 0x100, 0x100);
+    regmap_update_bits(wm8960->regmap, WM8960_LADC, 0x100, 0x100);
+    regmap_update_bits(wm8960->regmap, WM8960_RADC, 0x100, 0x100);
+    regmap_update_bits(wm8960->regmap, WM8960_LDAC, 0x100, 0x100);
+    regmap_update_bits(wm8960->regmap, WM8960_RDAC, 0x100, 0x100);
+    regmap_update_bits(wm8960->regmap, WM8960_LOUT1, 0x100, 0x100);
+    regmap_update_bits(wm8960->regmap, WM8960_ROUT1, 0x100, 0x100);
+    regmap_update_bits(wm8960->regmap, WM8960_LOUT2, 0x100, 0x100);
+    regmap_update_bits(wm8960->regmap, WM8960_ROUT2, 0x100, 0x100);
 
-	i2c_set_clientdata(i2c, wm8960);
+    i2c_set_clientdata(client, wm8960);
 
-	ret = devm_snd_soc_register_component(&i2c->dev,
-			&soc_component_dev_wm8960, &wm8960_dai, 1);
+    ret = devm_snd_soc_register_component(&client->dev, &soc_component_dev_wm8960, &wm8960_dai, 1);
 
-	return ret;
+    return ret;
 }
+
 
 static void wm8960_i2c_remove(struct i2c_client *client)
 {}
